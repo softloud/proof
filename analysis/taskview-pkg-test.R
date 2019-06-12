ae464310294fdc837dc7fedbe7a9e46256c43a72
cantabile <charlestigray@gmail.com>
2019-06-13 07:51:54 +1000

    eek should be on a branch

--- analysis/taskview-pkg-test.R
+++ analysis/taskview-pkg-test.R
@@ -4,53 +4,53 @@ library(lubridate)
 conflicted::conflict_prefer("filter", "dplyr")
 
 # get view metadata
-# tv <- available.views()
+# tv <- available.views() # takes a while
 
 # package list
-dat <- tv %>% 
-  transpose()  
+dat <- tv %>%
+  transpose()
 
 # tag with taskview identifier and bind in one df
-taskview_pkg_id <- dat %>% 
-  as_tibble() %>% 
+taskview_pkg_id <- dat %>%
+  as_tibble() %>%
   mutate(pkg = map2(name, packagelist, .f = function(x, y){
-    y %>% 
+    y %>%
       mutate(taskview = x,
              no_pkgs_in_tv = nrow(y))
-  })) %>% 
-  pluck("pkg") %>% 
-  bind_rows() %>% 
+  })) %>%
+  pluck("pkg") %>%
+  bind_rows() %>%
   rename(taskview_core = core)
 
 # get pkg
 pkg_test_size_desc <- read_rds("analysis/pkg_test_size_desc.rds")
 
 # bind it to testing data
-pkg_test_taskview <- pkg_test_size_desc %>% 
-  rename(name = cran_pkg_names) %>% 
-  full_join(taskview_pkg_id, by = "name") %>% 
+pkg_test_taskview <- pkg_test_size_desc %>%
+  rename(name = cran_pkg_names) %>%
+  full_join(taskview_pkg_id, by = "name") %>%
   mutate(no_tests = test_size_ratio == 0)
 
 # get dates
-dates <- pkg_test_taskview %>% 
+dates <- pkg_test_taskview %>%
   filter(!is.na(date)) %>% # not sure if date is the correct field
-  pluck("date") %>% 
-  as_date() %>% 
+  pluck("date") %>%
+  as_date() %>%
   discard(is.na) # why are there nas?
 
 # create plot data
-plot_data <- seq.Date(min(dates), max(dates), by = "month") %>% 
+plot_data <- seq.Date(min(dates), max(dates), by = "month") %>%
   map_df(.f = function(x){
-    pkg_test_taskview %>% 
-      filter(date >= x) %>% 
-      group_by(taskview) %>% 
-      summarise(no_tests_prop = sum(no_tests)/length(no_tests)) %>% 
-      mutate(date = as_date(x)) 
-  }) 
-  
+    pkg_test_taskview %>%
+      filter(date >= x) %>%
+      group_by(taskview) %>%
+      summarise(no_tests_prop = sum(no_tests)/length(no_tests)) %>%
+      mutate(date = as_date(x))
+  })
+
 # plot
 plot_data %>%
-  filter(!is.na(taskview )) %>% 
+  filter(!is.na(taskview )) %>%
   ggplot(aes(x = date, y = no_tests_prop)) +
   geom_line(aes(group = taskview, colour = taskview)) +
   labs(x = "Month",
@@ -61,7 +61,7 @@ ggsave("taskviews.png")
 # zoom plot
 plot_data %>%
   filter(!is.na(taskview),
-         no_tests_prop > 0.2) %>% 
+         no_tests_prop > 0.2) %>%
   ggplot(aes(x = date, y = no_tests_prop)) +
   geom_line(
     alpha = 0.8,
@@ -74,13 +74,33 @@ plot_data %>%
 ggsave("taskviews-zoom.png")
 
 
-# 
-pkg_test_taskview %>% 
-  filter(!is.na(taskview), !is.na(no_tests)) %>% 
-  group_by(taskview) %>% 
+#
+taskview_dat <- pkg_test_taskview %>%
+  filter(!is.na(taskview), !is.na(no_tests)) %>%
+  group_by(taskview)
+
+# relevel
+ts_levels <- taskview_dat %>%
+  group_by(taskview) %>%
+  select(name, no_tests) %>%
+  summarise(prop = sum(no_tests)/length(no_tests)) %>%
+  arrange(prop) %>%
+  pluck("taskview")
+
+# barcharts
+taskview_dat %>%
+  ungroup() %>%
+  mutate(taskview = fct_relevel(taskview, ts_levels)) %>%
   ggplot(aes(x = taskview, fill = no_tests)) +
   geom_bar() +
   coord_flip() +
-  scale_fill_grey()
+  scale_fill_grey("No tests") +
+  labs(
+    title = "How many packages in a taskview have no tests?",
+    x = "Number of packages in taskview",
+    y = "Taskview",
+    caption = "Taskviews ordered by proportion of packages with no tests."
+  )
 
 
+#
